9538e87272a96ab38ec1a85583dbde9c2a27a549
emir <emirariomatic@gmail.com>
2024-05-20 22:48:26 +0300

    UI structure complete (almost)

--- ToDo-List/ContentView.swift
+++ ToDo-List/ContentView.swift
@@ -6,16 +6,53 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    @State var newItemText = ""
+    
+    @Query var items: [TodoItem]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Add a task!", text: $newItemText)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button(action: {
+                        guard !newItemText.isEmpty else { return }
+                        let newTodo = TodoItem(title: newItemText, subtitle: "This is a subtitle.")
+                        context.insert(newTodo)
+                        newItemText = ""
+                    }, label: {
+                        Image(systemName: "plus.app.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 30)
+                    })
+                }
+                .padding(20)
+                
+                List {
+                    ForEach(items) { item in
+                        Text(item.title)
+                            .font(.headline)
+                        Text(item.subtitle)
+                            .font(.footnote)
+                    }
+                }
+                .overlay {
+                    if items.isEmpty {
+                        Text("You don't have any task, great!")
+                            .opacity(0.5)
+                    }
+                }
+            }
+            .navigationTitle("ToDo List")
+            
         }
-        .padding()
     }
 }
 
