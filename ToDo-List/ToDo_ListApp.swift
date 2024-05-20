d16bc544f3f14c85b0153e617b0f8b000944778a
emir <emirariomatic@gmail.com>
2024-05-20 22:03:00 +0300

    Initial Commit

---
+++ ToDo-List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo-List
+//
+//  Created by Emir Arı on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
